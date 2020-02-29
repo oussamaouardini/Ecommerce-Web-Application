c83b690a501242577f72a26694f8dc9ab955413d
3aitana <mohaamed.ouballa@gmail.com>
2020-02-29 18:34:28 +0100

    cardScroll

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -133,7 +133,7 @@
                             </a>
                             <div class="cart-hover">
                                 <div class="select-items">
-                                    <table>
+                                    <table id = "cardTable">
                                         <tbody>
                                         <tr>
                                             <td class="si-pic"><img src="images/img/select-product-1.jpg" alt=""></td>
@@ -147,6 +147,55 @@
                                                 <i class="ti-close"></i>
                                             </td>
                                         </tr>
+                                        <tr>
+                                            <td class="si-pic"><img src="images/img/select-product-2.jpg" alt=""></td>
+                                            <td class="si-text">
+                                                <div class="product-selected">
+                                                    <p>$60.00 x 1</p>
+                                                    <h6>Kabino Bedside Table</h6>
+                                                </div>
+                                            </td>
+                                            <td class="si-close">
+                                                <i class="ti-close"></i>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="si-pic"><img src="images/img/select-product-2.jpg" alt=""></td>
+                                            <td class="si-text">
+                                                <div class="product-selected">
+                                                    <p>$60.00 x 1</p>
+                                                    <h6>Kabino Bedside Table</h6>
+                                                </div>
+                                            </td>
+                                            <td class="si-close">
+                                                <i class="ti-close"></i>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="si-pic"><img src="images/img/select-product-2.jpg" alt=""></td>
+                                            <td class="si-text">
+                                                <div class="product-selected">
+                                                    <p>$60.00 x 1</p>
+                                                    <h6>Kabino Bedside Table</h6>
+                                                </div>
+                                            </td>
+                                            <td class="si-close">
+                                                <i class="ti-close"></i>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="si-pic"><img src="images/img/select-product-2.jpg" alt=""></td>
+                                            <td class="si-text">
+                                                <div class="product-selected">
+                                                    <p>$60.00 x 1</p>
+                                                    <h6>Kabino Bedside Table</h6>
+                                                </div>
+                                            </td>
+                                            <td class="si-close">
+                                                <i class="ti-close"></i>
+                                            </td>
+                                        </tr>
+
                                         <tr>
                                             <td class="si-pic"><img src="images/img/select-product-2.jpg" alt=""></td>
                                             <td class="si-text">
